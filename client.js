b3fa2c0d7cc156dda8b81bf1b5ff1fa9a90b0d18
Leighton Turner <leightonturner@gmail.com>
2013-12-04 11:12:18 +1300

    Snapshot and image destruction support and some API refactoring

--- client.js
+++ client.js
@@ -1,35 +1,50 @@
 
 var api     = require("./lib/api.js")
   , cred    = require("./etc/api-key.json")
-  , program = require('commander');
+  , _       = require("underscore")
+  , program = require('commander')
+  , Table   = require('cli-table');
 
 var bind_hander= function(fn, handler){
   return function(){
     var args = Array.prototype.slice.call(arguments);
     //rather fortunate the last argument is commander details
     //make it the handler instead
-    args[args.length-1]=handler;
+    var commander_arg = args[args.length-1];
+    args[args.length-1] = function(){ //bind handler as callback to API
+      var a = Array.prototype.slice.call(arguments);
+      a.push(commander_arg); //bind commander argument to handler
+      handler.apply(handler,a);
+    }
     fn.apply(fn,args);
   }
 };
 
-var print_info = function(json){
+var print_status = function(json,cmd){
   if(json.droplets){
-    console.log("\nID\tNAME\t\tSTATUS");
-    json.droplets.forEach(function(x){
-      console.log(x.id + "\t" + x.name + "\t" + x.status);
+    
+    var table = new Table({
+      head: ['ID', 'NAME', 'STATUS'], 
+      colWidths: [10,20,10]
     });
-    console.log("");
+    
+    _.each(json.droplets,function(x){
+      table.push(
+        [x.id, x.name, x.status]
+      );
+    });
+    
+    console.log(table.toString()); 
   }
 };
 program
-  .command('info')
-  .description('Droplet information')
-  .action(bind_hander(api.info, print_info));
+  .command('status')
+  .description('Status of your droplets')
+  .action(bind_hander(api.status, print_status));
 
 program
   .command('power <name> <state>')
-  .description('Power off or on droplet')
+  .description('Power (on|off) droplet')
   .action(bind_hander(api.power,console.log));
 
 program
@@ -37,16 +52,52 @@ program
   .description('Shutdown droplet')
   .action(bind_hander(api.shutdown,console.log));
 
+program
+  .command('snapshot <droplet_name> <snapshot_name>')
+  .description('Snapshot droplet')
+  .action(bind_hander(api.snapshot,console.log));
+
+/**
+ * Images API
+ */
+var print_images = function(json,cmd){
+  
+  if(json.images){
+    var table = new Table({
+      head: ['ID', 'PUBLIC', 'OS', 'NAME'], 
+      colWidths: [10,10,15,30]
+    });
+
+    _.each(json.images,function(x){
+      if(cmd.all){
+        table.push(
+          [x.id, x.public, x.distribution, x.name]
+        );
+      }else{
+        if(!x.public){
+          table.push(
+            [x.id, x.public, x.distribution, x.name]
+          );
+        }
+      }
+    });
+    
+    console.log(table.toString());
+  }
+};
+program
+  .command('images')
+  .description('Droplet images')
+  .option('-a, --all', 'Print all images not just private')
+  .action(bind_hander(api.images, print_images));
+
+program
+  .command('destroy-image <id>')
+  .description('Destroy image')
+  .action(bind_hander(api.destroy_image, console.log));
+
+
 api.init(cred, function(){
   program.parse(process.argv);
 });
-
-
-
-
-
-
-
-
-
 
